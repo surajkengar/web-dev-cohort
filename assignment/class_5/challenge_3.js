a9f748a20a7e20877a2d51e606b4780384aa1546
suraj kengar <surajkengar1212@gmail.com>
2025-08-26 23:32:52 +0530

    add js code

---
+++ assignment/class_5/challenge_3.js
@@ -0,0 +1,16 @@
+function print(){
+    console.log("hello coders in web cohort");
+
+}
+
+print();
+
+
+function add(num1 , num2){
+    return num1 + num2;
+}
+
+let temp = add(4 , 3);
+console.log("addition of that two number is " + temp);
+
+
